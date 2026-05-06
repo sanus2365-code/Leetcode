2fd1b9c67f4eb48b6e469a2944d674b27d52e57e
NIBIL <sanus2365@gmail.com>
2026-05-06 10:47:30 +0530

    Time: 189 ms (42.43%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,9 @@
+-- Write your PostgreSQL query statement below
+select
+case
+when id % 2 = 1 and id !=(select max(id) from seat) then id + 1
+when id % 2 = 0 then id - 1
+else id
+end as id, student
+from Seat
+order by id;
