2103d7c523338699498d32e717c2d53d68044c6e
NIBIL <sanus2365@gmail.com>
2026-05-07 16:18:56 +0530

    Time: 294 ms (21.82%), Space: 0B (100%) - LeetHub

---
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+WITH uniq_coords AS (
+  SELECT *, 
+    COUNT(*) OVER (PARTITION BY lat, lon) AS attempts,
+    COUNT(*) OVER (PARTITION BY tiv_2015) AS tivs
+  FROM Insurance
+)
+SELECT ROUND(SUM(tiv_2016)::numeric, 2) AS tiv_2016
+FROM uniq_coords
+WHERE attempts = 1 AND tivs > 1;
